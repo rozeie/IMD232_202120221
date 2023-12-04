6d4f9834f39f3bf1c5f1eb67752249b4b5a7b24e
rozeie <139133572+rozeie@users.noreply.github.com>
2023-12-04 11:04:35 +0900

    1204

--- src/final/sketches/sketch1/script/jellyfish.js
+++ src/final/sketches/sketch1/script/jellyfish.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(500, 700, WEBGL);
   angleMode(DEGREES);
 }
 
@@ -11,13 +11,19 @@ function draw() {
   noFill();
   stroke(255);
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 20; i++) {
+    var r = map(sin(frameCount / 2), -1, 1, 0, 255);
+    var g = map(i, 0, 20, 100, 200);
+    var b = map(cos(frameCount), -1, 1, 200, 100);
+
+    stroke(r, g, b);
+
     beginShape();
     for (var j = 0; j < 360; j += 10) {
       var rad = i * 8;
       var x = rad * cos(j);
       var y = rad * sin(j);
-      var z = sin(frameCount) * 50;
+      var z = sin(frameCount * 2 + i * 10) * 50;
 
       vertex(x, y, z);
     }
